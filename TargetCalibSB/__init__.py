8b4bdcaeec8f2f14a473e9fbba32ff38880e8c7e
watsonjj <jason.jw@live.co.uk>
2019-08-29 17:54:44 +0200

    Add setup

---
+++ TargetCalibSB/__init__.py
@@ -0,0 +1,1 @@
+N_BLOCKPHASE = 32
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+PACKAGENAME = "TargetCalibSB"
+DESCRIPTION = (
+    "Python package to explore calibration approaches for TARGET modules"
+)
+AUTHOR = "Jason J Watson"
+AUTHOR_EMAIL = "jason.watson@physics.ox.ac.uk"
+VERSION = "0.0.1"
+
+setup(
+    name=PACKAGENAME,
+    packages=find_packages(),
+    version=VERSION,
+    description=DESCRIPTION,
+    license='BSD3',
+    install_requires=[
+        'astropy',
+        'scipy',
+        'numpy',
+        'matplotlib',
+        'tqdm',
+        'pandas>=0.21.0',
+        'iminuit',
+        'numba',
+        'PyYAML',
+        'packaging',
+        'CHECLabPy',
+    ],
+    setup_requires=['pytest-runner', ],
+    tests_require=['pytest', ],
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+)
