d5c0f33111ab38e501ba5ef9416137c05dd04160
watsonjj <jason.jw@live.co.uk>
2019-10-30 19:40:05 +0100

    Create convenience methods for converting to cell_ids

--- TargetCalibSB/__init__.py
+++ TargetCalibSB/__init__.py
@@ -1,1 +1,23 @@
+import numpy as np
+from numba import njit
+
 N_BLOCKPHASE = 32
+N_ROWS = 8
+
+
+def calculate_row_column_blockphase(cell_id):
+    blockphase = cell_id % N_BLOCKPHASE
+    row = (cell_id // N_BLOCKPHASE) % N_ROWS
+    column = (cell_id // N_BLOCKPHASE) // N_ROWS
+    return row, column, blockphase
+
+
+@njit
+def get_cell_ids_for_waveform(first_cell_id, n_samples, n_cells):
+    isample = np.arange(n_samples)
+    block = first_cell_id // N_BLOCKPHASE
+    blockphase = first_cell_id % N_BLOCKPHASE
+    factor = 64 * (1 - 2 * (block % 2))
+    shift = (((blockphase + isample) // N_BLOCKPHASE) % 2) * factor
+    cell = (first_cell_id + isample + shift) % n_cells
+    return cell
